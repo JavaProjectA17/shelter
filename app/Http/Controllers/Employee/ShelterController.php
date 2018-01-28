4d345f6ed48d99ea24c468925172b2b86c2b844b
RAnastasiya <rizhko.anastasiya@gmail.com>
2018-01-28 15:23:17 +0200

    add any blabla

---
+++ app/Http/Controllers/Employee/ShelterController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Employee;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Shelter;
+use Auth;
+
+class ShelterController extends Controller
+{
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('main.add_new_shelter');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function send_form(Request $request)
+    {
+//        dd(Auth::user());
+//        dd($request);
+//        echo "fsdfsdf";
+        return view('main.add_new_shelter.success');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
