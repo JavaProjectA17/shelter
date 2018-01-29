adc21e1c508cbb5f5b3afe442b91acdec3d0e7c9
RAnastasiya <rizhko.anastasiya@gmail.com>
2018-01-29 19:50:45 +0200

    added hiding text

--- resources/views/main/add_new_shelter/form.blade.php
+++ resources/views/main/add_new_shelter/form.blade.php
@@ -1,8 +1,8 @@
+@if (session('status'))
+    <h2 class="response-status">{{ session('status') }}</h2>
+@endif
+<h2 class="ic1 form-new-shelter">Form of adding a new cattery</h2>
 <div class="grid_5 prefix_1">
-    @if (session('status'))
-        <h2 class="response-status">{{ session('status') }}</h2>
-    @endif
-    <h2 class="ic1 form-new-shelter">Form of adding a new cattery</h2>
     {!! Form::open(['url' => 'send_form', 'id'=>'form', 'method'=>'POST']) !!}
     <div class="form-inputs">
         {{ Form::text("nameshelter", $value = null, ['placeholder' => 'Enter the name of the shelter']) }}
