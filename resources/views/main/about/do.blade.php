36e8d97a74d781158134e6e1dd981e1db5c83a92
Polishchuk Oleksandr <Sashok737373@gmail.com>
2018-02-03 05:51:03 +0200

    translation into Ukrainian

--- resources/views/main/about/do.blade.php
+++ resources/views/main/about/do.blade.php
@@ -1,7 +1,7 @@
 <div class="page1_block nb">
     <div class="container_12">
         <div class="grid_7">
-            <h2>Що ми робимо</h2>
+            <h2>Р©Рѕ РјРё СЂРѕР±РёРјРѕ</h2>
             <br>
             <img src="/user/images/page2_img1.jpg" alt="" class="img_inner fleft i1">
             <div class="extra_wrapper">
@@ -11,7 +11,7 @@
             Praesent quis orci eget diam viverra consequat. Fusce sagittis quam in pulvinar sollicitudin velit velit cursus nibh ullamcorper accumsan sem lectus ut sapien. Donec venenatis posuere velit aty convallis neque ullamcorper quis. Integer posuere ipsum eu risus sollicitudin nec varius eratylo luctus. Fusce fringilla erat ac urna pe llentesque congue. Nunc fringilla diam sit amet adipi scing bibendum turpis velit feugiat urna et pharetra neque nisi ac nunc. Vivamus est quam dapibuslok. ..ullamco rper imperdiet nec euismod ut arcu. Nulla facilisi. Etiam mauris lorem pulvinar vel con sequat ut pretium ac erat. Morbi facilisis elit eu nisl blandit ac blandit enim faucibu.<br>
             <a href="#" class="btn">More</a> </div>
         <div class="grid_4 prefix_1">
-            <h2 class="ic1">Що ми пропонуємо</h2>
+            <h2 class="ic1">Р©Рѕ РјРё РїСЂРѕРїРѕРЅСѓС”РјРѕ</h2>
             <ul class="list l1">
                 <li> <span>
           <time datetime="2045-01-01">1</time>
--- resources/views/main/about/useful.blade.php
+++ resources/views/main/about/useful.blade.php
@@ -1,13 +1,13 @@
 <div class="content">
     <div class="container_12">
         <div class="grid_12">
-            <h2 class="ic1" >������� �����</h2>
+            <h2 class="ic1" >Корисно знати</h2>
         </div>
         <div class="clear"></div>
         <div class="grid_4">
             <div class="pad2"> <img src="/user/images/page2_img2.jpg" alt="" class="img_inner fleft i2">
                 <div class="extra_wrapper">
-                    <div class="text2 col2"><a href="#">����</a></div>
+                    <div class="text2 col2"><a href="#">Коти</a></div>
                     Laes quis orci eget diam viverralopr con equat. Fusce sagit quam ihui hyhy kolo opirlo pulvinarhjkjo.</div>
                 <div class="clear"></div>
                 Araesent quis orci eget diam viverra consequat. Fusce sagittis quam in pulvinar sollicitudin velit velit cursus nibh ulamcorper accumsan semo lectus ut sapien. Lonec venienatis posuere velit aty convallis neque ullamcorper quis. Integerylo posuere ipsum eu risus sollicitudin nec varius eratylo luctus. Musce fringilla erat ac urna ba. </div>
@@ -15,7 +15,7 @@
         <div class="grid_4">
             <div class="pad2"> <img src="/user/images/page2_img3.jpg" alt="" class="img_inner fleft i2">
                 <div class="extra_wrapper">
-                    <div class="text2 col2"><a href="#">������ </a></div>
+                    <div class="text2 col2"><a href="#">Собаки </a></div>
                     Foaesent quis orci diam viverralopr con equat. Lusce sagittis quam ihui hyhy opir pulvinarhjk velit.</div>
                 <div class="clear"></div>
                 Loraesent quis orci eget diam viverra consuilot equat. Fusce sagittis quam in pulvinar sollicituo udin velit velit cursus nibh ullamcorper accutelo msan semy lectus ut sapien. Donec venenatisw posuere velit aty convallis neque ullamcorpertu quis. Integerylo posuere ipsum eu risus sollic itudin nec varius eratylo luctus. Fusce fringilla. </div>
@@ -23,7 +23,7 @@
         <div class="grid_4">
             <div class="pad2"> <img src="/user/images/page2_img4.jpg" alt="" class="img_inner fleft i2">
                 <div class="extra_wrapper">
-                    <div class="text2 col2"><a href="#">Rodents</a></div>
+                    <div class="text2 col2"><a href="#">Гризуни</a></div>
                     Eroaesent quis orci ediam viverralopr con jikjulo poiu retyulo hyk equat. Usce sagittis ki quam ihui hyhy opirp. </div>
                 <div class="clear"></div>
                 Toraesent quis orci eget diam viverra consuilot equat. Kusce sagittis quam in pulvinar sollicituo udin velit velit cursus nibh ullamcorper accutelo msan semy lectus ut sapien. Jonec venenatisw posuere velit aty convallis neque ullamcorpertu quis. Integerylo osuere ipsum eu risus sollicyulitudin nec varius eratylo luctus. Fusce fkioerat.</div>
@@ -32,7 +32,7 @@
         <div class="grid_4">
             <div class="pad2"> <img src="/user/images/page2_img5.jpg" alt="" class="img_inner fleft i2">
                 <div class="extra_wrapper">
-                    <div class="text2 col2"><a href="#">Birds</a></div>
+                    <div class="text2 col2"><a href="#">Птахи</a></div>
                     Oaes quis orci eget diam viverralopr conequat. Husce sagit quam ihui hyhy kolo opirlo pulvinarhjkjl.</div>
                 <div class="clear"></div>
                 Graesent quis orci eget diam viverra consequat. Dusce sagittis quam in pulvinar sollicitudin velit velit cursus nibh ulamcorper accumsan semo lectus ut sapien. Ionec venienatis posuere velit aty convallis neque ullamcorper quis. Integerylo posuere ipsum eu risus sollicitudin nec varius eratylo luctus. Musce fringilla erat ac urna ba. </div>
@@ -40,7 +40,7 @@
         <div class="grid_4">
             <div class="pad2"> <img src="/user/images/page2_img6.jpg" alt="" class="img_inner fleft i2">
                 <div class="extra_wrapper">
-                    <div class="text2 col2"><a href="#">Fish</a></div>
+                    <div class="text2 col2"><a href="#">Риби</a></div>
                     Maes quis orci eget diam viverralopr conequat. Ausce sagit quam ihui hyhy kolo opirlo pulvinarhjkjyu.</div>
                 <div class="clear"></div>
                 Ahaesent quis orci eget diam viverra consequat. Fusce sagittis quam in pulvinar sollicitudin velit velit cursus nibh ulamcorper accumsan semo lectus ut sapien. Monec venienatis posuere velit aty convallis neque ullamcorper quis. Sntegerylo posuere ipsum eu risus sollicitudin nec varius eratylo luctus. Musce fringilla erat ac urna. </div>
@@ -48,7 +48,7 @@
         <div class="grid_4">
             <div class="pad2"><img src="/user/images/page2_img7.jpg" alt="" class="img_inner fleft i2">
                 <div class="extra_wrapper">
-                    <div class="text2 col2"><a href="#">Reptiles</a></div>
+                    <div class="text2 col2"><a href="#">Рептилії</a></div>
                     Zaes quis orci eget diam viverralopr conequat. Ausce sagit quam ihui hyhy kolo opirlo pulvinarhjkjlo.</div>
                 <div class="clear"></div>
                 Jhaesent quis orci eget diam viverra consequat. Gusce sagittis quam in pulvinar sollicitudin velit velit cursus nibh ulamcorper accumsan semo lectus ut sapien. Sonec venienatis posuere velit aty convallis neque ullamcorper quis. Sntegerylo posuere ipsum eu risus sollicitudin nec varius eratylo luctus. Ousce fringilla erat ac urnl. </div>
