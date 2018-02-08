672ea71a03537887b74b23ff5f6e3c1998994d90
oleg <trigyba.oleg@gmail.com>
2018-02-08 21:11:46 +0200

    3 commit HEROKU change All

--- config/database.php
+++ config/database.php
@@ -39,13 +39,13 @@ return [
             'prefix' => '',
         ],
 
-        'pgsql' => [
+        'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'ec2-50-19-105-188.compute-1.amazonaws.com'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'd44ign0rprlp69'),
-            'username' => env('DB_USERNAME', 'ncuizjbfsrolaq'),
-            'password' => env('DB_PASSWORD', '89da7e4323771f6ecd93305672b8e13374da28de3e42b916398c8bcf2360ca86'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-50-19-105-188.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd44ign0rprlp69'),
+            'username' => env('DB_USERNAME', 'ncuizjbfsrolaq'),
+            'password' => env('DB_PASSWORD', '89da7e4323771f6ecd93305672b8e13374da28de3e42b916398c8bcf2360ca86'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
